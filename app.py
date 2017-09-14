c9f25655de31fbeb8a371699d77b6cbb416d7735
vicabitu <victor.ch370@gmail.com>
2017-09-13 23:43:21 -0300

    Primer commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "prueba ok"
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
