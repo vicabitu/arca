2ee4eadb7eb034934381263e00419f53771072a8
vicabitu <victor.ch370@gmail.com>
2017-09-19 16:15:29 -0300

    agregado de estaticos

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "prueba ok"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
